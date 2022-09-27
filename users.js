485e640c19e632551c6d8dde27da03fd23d6a413
figgyjay <79807654+figgyjay@users.noreply.github.com>
2022-09-27 13:16:55 -0500

    Assignment 2 Validation blogs/users.js folder

--- users.js
+++ users.js
@@ -1,91 +1,75 @@
-var express = require('express');
-var router = express.Router();
+const validateUserData = (userData) => {
 
-var { validateUserData } = require("../validation/users");
-
-const userList = [];
-
-
-/* GET users listing. */
-router.get('/all', function(req, res, next) {
-  res.send('respond with a resource');
-});
-
-router.get('/single', (req, res) => {
-
-  res.json ({
-    success: true,
-    user: "Single User"
-  })
-})
-
-router.post("/create-one", (req, res) => {
-  try{
-  
-    const email = req.body.email;
-    const firstName = req.body.firstName;
-    const lastName = req.body.lastName;
-    const age = req.body.age;
-    const favoriteFoods = req.body.favoriteFoods;
-  
-        favoriteFoods.forEach(() => {});
-  
-
-  
+    if (userData.email === undefined || typeof(userData.email) !== "string") {
+        return {
+            isValid: false,
+            message: "Email is required and it must be a string"
+        }
+    }
 
 
-    const userData = {
-      email,
-      firstName,
-      lastName,
-      age,
-      favoriteFoods,
-      fullName: firstName + lastName,
-      createdAt: new Date(),
-      lastModified: new Date(),
-    };
-  
-  
+    if (userData.firstName === undefined || typeof(userData.firstName) !== "string") {
+        return {
+            isValid: false,
+            message: "First name is required and it must be a string"
+        }
+    }
 
-    const userDataCheck = validateUserData(userData);
-    
-    if (userDataCheck.isValid === false) {
+    if (userData.lastName === undefined || typeof(userData.lastName) !== "string") {
+        return {
+            isValid: false,
+            message: "Last name is required and it must be a string"
+        }
+    }
 
-      throw Error(userDataCheck.message)
+    if (userData.age !== undefined && typeof(userData.age) !== "number") {
 
-       res.json({
-        success: false,
-        message: userDataCheck.message,
-      });
+        return {
+            isValid: false,
+            message: "Age must be a number"
+        }
+    }
+    if (userData.favoriteFoods === undefined || !Array.isArray(userData.favoriteFoods) || userData.favoriteFoods.length === 0) {
 
-      return;
-    };
+        return {
+            isValid: false,
+            message: "favorite foods must be an array and must have length"
+        }
+    }
 
-      userList.push(userData);
+    const nonStringFoods = userData.favoriteFoods.filter((favoriteFood)=>{
 
-      console.log("userList ", userList);
+        if (typeof(favoriteFood) !== 'string') {
 
-      res.json({
-        success: true,
-      });
+            return true
+             } else {
+                return false
+             }
+            })
 
-    } catch (e) {
-      console.log(e);
-    };
+            console.log("nonStringFoods ", nonStringFoods);
 
-      res.json({
-        success: false,
-        error: String(e)
-      });
-    
-});
-  
+            if (nonStringFoods.length > 0) {
 
-  
+                return {
+                    isValid: false,
+                    message: "favorite foods must be an array of strings"
+                }
+            }
+            return {
+                isValid: true
+            }
+        }
+
+        module.exports = {
+
+         validateUserData,
+        };
 
 
 
 
 
 
- module.exports = router;
+
+
